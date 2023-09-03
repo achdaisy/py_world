2a5be85f0428da54b33a48583f6e5740770face2
achdaisy <oduordaizy@gmail.com>
2023-09-03 12:23:52 +0300

    added my python file

---
+++ madlibs_generator/madlibs.py
@@ -0,0 +1,19 @@
+print ("Madlibs generator")
+print("----------------------------------------------")
+
+the_noun = input("Enter the noun: ")
+the_pronoun = input("Input the pronoun: ")
+sec_noun = input("Please enter another noun: ")
+place = input("Choose a place: ")
+adjective = input("Choose an adjective(describing word): ")
+third_noun = input("Enter another noun: ")
+
+
+print("-----------------------------------------------")
+print("Be kind to your", the_noun, "-footed", the_pronoun)
+print("For a duck may be somebody\'s ", sec_noun, ",")
+print("Be kind to your ", the_pronoun, "in" , place )
+print("Where the weather is always ", adjective, ".\n")
+print("You may think this is the ", third_noun, ",")
+print("Well it is")
+print("-----------------------------------------------")
